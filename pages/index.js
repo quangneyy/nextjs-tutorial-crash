a5ef9e1ca65f29e5cac577eaa2f4b20c49e00720
QuangNey <quangney12602@gmail.com>
2022-06-03 11:42:05 +0700

    link-nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Layout from '../components/Layout'
+import Link from 'next/link'
 
 export default function Home() {
   return <Layout>
     <h1>My Next App</h1>
+    <Link href='/about'>ABOUT PAGE</Link>
   </Layout>
 }
